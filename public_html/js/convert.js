197bca3c5d99a9ca4b913ce65c8e2b2c6995bf1f
rpre <sovetsky-soyuz@yandex.ru>
2016-06-15 07:48:16 +0300

    remove IE8 support, fix the bugs

--- public_html/js/convert.js
+++ public_html/js/convert.js
@@ -1,38 +1,65 @@
 //copyright http://sovietera.net (ussr.km.ru) - пожалуйста, не крадите эту и другие страницы, а ставьте ссылку.
 
-var RUS = "абвгдеёжзийклмнопрстуфхцчшщъьыэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЬЫЭЮЯ\"№;:?/.,",
-    ENG = "f,dult`;pbqrkvyjghcnea[wxio]ms'.zF<DULT~:PBQRKVYJGHCNEA{WXIO}MS'>Z\@#$^&|/?";
-var FROM = "",
-    TO = "";
+var rus = "абвгдеёжзийклмнопрстуфхцчшщъьыэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЬЫЭЮЯ\"№;:?/.,",
+    eng = "f,dult`;pbqrkvyjghcnea[wxio]ms'.zF<DULT~:PBQRKVYJGHCNEA{WXIO}MS'>Z\@#$^&|/?";
+var fromLang = "",
+    toLang = "";
 
-function convert(f) {
-    s = f.coded.value;
-    if (f.lang.selectedIndex === 0) {
-        if (RUS.indexOf(s.charAt(1)) === -1) {
-            FROM = ENG;
-            TO = RUS;
-        } else {
-            FROM = RUS;
-            TO = ENG;
-        }
-    } else if (f.lang.selectedIndex === 1) {
-        FROM = ENG;
-        TO = RUS;
-    } else if (f.lang.selectedIndex === 2) {
-        FROM = RUS;
-        TO = ENG;
+function convert(text, lang) {
+    var resultText = "";
+    switch (lang) {
+        case 0:
+            if (rus.indexOf(text.charAt(1)) === -1) {
+                fromLang = eng;
+                toLang = rus;
+            } else {
+                fromLang = rus;
+                toLang = eng;
+            }
+            break;
+        case 1:
+            fromLang = eng;
+            toLang = rus;
+            break;
+        case 2:
+            fromLang = rus;
+            toLang = eng;
+            break;
     }
-    c = "";
-    for (var i = 0; i < s.length; i++) {
-        var j = FROM.indexOf(s.charAt(i));
+    for (var i = 0; i < text.length; i++) {
+        var j = fromLang.indexOf(text.charAt(i));
         if (j < 0) {
-            c += s.charAt(i);
+            resultText += text.charAt(i);
         } else {
-            c += TO.charAt(j);
+            resultText += toLang.charAt(j);
         }
     }
-    // return c;
-    f.decoded.value = c;
+    return resultText;
+
     //document.write(f.decoded.value);
 }
+
+function output(form) {
+    form.decoded.value = convert(form.coded.value, form.lang.selectedIndex);
+}
+
+function input(form) {
+    area = form.coded;
+    button = document.getElementById('start-btn');
+
+    area.addEventListener('input', function() {
+        output(form);
+    }, false);
+    button.addEventListener("click", output(form), false);
+}
+
 //- сделать нормальную обработку кавычек и двойного переключения раскладки
+
+function foo() {
+    var form = document.forms[0];
+    var button = document.getElementById('start-btn');
+
+    input(form);
+}
+
+window.onload = foo;
