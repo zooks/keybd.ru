f37dad2683592bebc666efe7839e13817fd37d47
rpre <sovetsky-soyuz@yandex.ru>
2017-10-27 03:58:46 +0300

    year update

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      exclude: /\/node_modules\//,
+      exclude: /node_modules/,
       loader: 'babel-loader?presets[]=es2015'
     }]
   }
