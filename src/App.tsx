446c838e2a49de68bb4628f1d3a4ec20ca02c3d3
zooks <onde_sonore@mail.ru>
2022-11-15 04:53:33 +0300

    add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {Route, Routes} from 'react-router-dom'
 import {MainPage} from "./pages/MainPage";
 import {EmojiPage} from "./pages/EmojiPage";
 import {TextCasePage} from "./pages/TextCasePage";
+import {NotFoundPage} from "./pages/NotFoundPage";
 import './App.scss';
 import {Navigation} from "./components/Navigation";
 import {Footer} from "./components/Footer";
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/" element={ <MainPage /> }></Route>
                 <Route path="/emoji" element={ <EmojiPage /> }></Route>
                 <Route path="/text-case" element={ <TextCasePage /> }></Route>
+                <Route path="*" element={ <NotFoundPage /> } />
             </Routes>
             <Footer />
         </>
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+
+export function NotFoundPage() {
+    return (
+        <main>
+            <Container>
+                <div>
+                    <h2>404 Страница не найдена</h2>
+                </div>
+            </Container>
+        </main>
+    );
+}
